c89814843bdea3f1c7db99a759afc687be196ab1
Hunter Parcells <hparcells@icloud.com>
2018-06-28 09:36:10 -0400

    Rectangle Area

---
+++ src/hunter/shapeMath/shapes/twoD/Rectangle.java
@@ -0,0 +1,57 @@
+package hunter.shapeMath.shapes.twoD;
+
+public class Rectangle {
+	/**
+	 * Finds the area of a rectangle given one of the side lengths.
+	 * @param sideLength The length of one of the sides.
+	 * @return The area of the rectangle.
+	 */
+	public static byte area(byte sideLength) {
+		return sideLength *= sideLength;
+	}
+	
+	/**
+	 * Finds the area of a rectangle given one of the side lengths.
+	 * @param sideLength The length of one of the sides.
+	 * @return The area of the rectangle.
+	 */
+	public static short area(short sideLength) {
+		return sideLength *= sideLength;
+	}
+	
+	/**
+	 * Finds the area of a rectangle given one of the side lengths.
+	 * @param sideLength The length of one of the sides.
+	 * @return The area of the rectangle.
+	 */
+	public static int area(int sideLength) {
+		return sideLength *= sideLength;
+	}
+	
+	/**
+	 * Finds the area of a rectangle given one of the side lengths.
+	 * @param sideLength The length of one of the sides.
+	 * @return The area of the rectangle.
+	 */
+	public static float area(float sideLength) {
+		return sideLength *= sideLength;
+	}
+	
+	/**
+	 * Finds the area of a rectangle given one of the side lengths.
+	 * @param sideLength The length of one of the sides.
+	 * @return The area of the rectangle.
+	 */
+	public static double area(double sideLength) {
+		return sideLength *= sideLength;
+	}
+	
+	/**
+	 * Finds the area of a rectangle given one of the side lengths.
+	 * @param sideLength The length of one of the sides.
+	 * @return The area of the rectangle.
+	 */
+	public static long area(long sideLength) {
+		return sideLength *= sideLength;
+	}
+}
