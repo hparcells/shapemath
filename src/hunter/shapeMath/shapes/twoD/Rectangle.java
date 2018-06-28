5042918c151fcb4a86b7a4fceb02f534cdc74ca3
Hunter Parcells <hparcells@icloud.com>
2018-06-28 16:13:42 -0400

    Perimeter

--- src/hunter/shapeMath/shapes/twoD/Rectangle.java
+++ src/hunter/shapeMath/shapes/twoD/Rectangle.java
@@ -1,5 +1,10 @@
 package hunter.shapeMath.shapes.twoD;
 
+/**
+ * All Rectangle Based Math.
+ * @author Hunter Parcells
+ *
+ */
 public class Rectangle {
 	/**
 	 * Finds the area of a rectangle given one of the side lengths.
@@ -53,5 +58,71 @@ public class Rectangle {
 	 */
 	public static long area(long sideLength) {
 		return sideLength *= sideLength;
+	}
+	
+	/**
+	 * Finds the perimeter of a rectangle given the area.
+	 * @param area The area of the rectangle.
+	 * @return The perimeter of the rectangle.
+	 */
+	public static byte perimeterByArea(byte area) {
+		byte sideLength = (byte) Math.sqrt(area);
+		
+		return sideLength *= 4;
+	}
+	
+	/**
+	 * Finds the perimeter of a rectangle given the area.
+	 * @param area The area of the rectangle.
+	 * @return The perimeter of the rectangle.
+	 */
+	public static short perimeterByArea(short area) {
+		short sideLength = (short) Math.sqrt(area);
+		
+		return sideLength *= 4;
+	}
+	
+	/**
+	 * Finds the perimeter of a rectangle given the area.
+	 * @param area The area of the rectangle.
+	 * @return The perimeter of the rectangle.
+	 */
+	public static int perimeterByArea(int area) {
+		int sideLength = (int) Math.sqrt(area);
+		
+		return sideLength *= 4;
+	}
+	
+	/**
+	 * Finds the perimeter of a rectangle given the area.
+	 * @param area The area of the rectangle.
+	 * @return The perimeter of the rectangle.
+	 */
+	public static float perimeterByArea(float area) {
+		float sideLength = (float) Math.sqrt(area);
+		
+		return sideLength *= 4;
+	}
+	
+	/**
+	 * Finds the perimeter of a rectangle given the area.
+	 * @param area The area of the rectangle.
+	 * @return The perimeter of the rectangle.
+	 */
+	public static double perimeterByArea(double area) {
+		double sideLength = Math.sqrt(area);
+		
+		return sideLength *= 4;
+	}
+	
+	/**
+	 * Finds the perimeter of a rectangle given the area.
+	 * @param area The area of the rectangle.
+	 * @return The perimeter of the rectangle.
+	 */
+	public static long perimeterByArea(long area) {
+		long sideLength = (long) Math.sqrt(area);
+		
+		return sideLength *= 4;
 	}
 }
