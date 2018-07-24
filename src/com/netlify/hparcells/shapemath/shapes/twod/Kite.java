f965593e4b191f1fe5010a0c69785280a44d3c79
Hunter Parcells <hparcells@icloud.com>
2018-07-24 14:55:36 -0400

    Kite and Trapezoid

---
+++ src/com/netlify/hparcells/shapemath/shapes/twod/Kite.java
@@ -0,0 +1,10 @@
+package com.netlify.hparcells.shapemath.shapes.twod;
+
+/**
+ * All Kite Based Math. Extends Rhombus.
+ * 
+ * @author Hunter Parcells
+ */
+public class Kite extends Rhombus {
+	//Extends Rhombus
+}
