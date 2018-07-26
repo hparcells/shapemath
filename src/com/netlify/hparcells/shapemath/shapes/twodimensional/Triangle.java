763521bdbc94314d2011d42da0e439bcf27b2c75
Hunter Parcells <hparcells@icloud.com>
2018-07-26 14:22:27 -0400

    Triangle missingInteriorAngle

--- src/com/netlify/hparcells/shapemath/shapes/twodimensional/Triangle.java
+++ src/com/netlify/hparcells/shapemath/shapes/twodimensional/Triangle.java
@@ -221,4 +221,64 @@ public class Triangle {
 		
 		return (long) Math.sqrt(hypotenuseSq - sideSq);
 	}
+	
+	/**
+	 * Finds the missing interior angle of a triangle.
+	 * @param angle1 The degrees of one of the given angles.
+	 * @param angle2 The degrees of the other given angle.
+	 * @return The degrees of the missing angle.
+	 */
+	public static byte missingInteriorAngle(byte angle1, byte angle2) {
+		return (byte) (180 - (angle1 + angle2));
+	}
+	
+	/**
+	 * Finds the missing interior angle of a triangle.
+	 * @param angle1 The degrees of one of the given angles.
+	 * @param angle2 The degrees of the other given angle.
+	 * @return The degrees of the missing angle.
+	 */
+	public static short missingInteriorAngle(short angle1, short angle2) {
+		return (short) (180 - (angle1 + angle2));
+	}
+	
+	/**
+	 * Finds the missing interior angle of a triangle.
+	 * @param angle1 The degrees of one of the given angles.
+	 * @param angle2 The degrees of the other given angle.
+	 * @return The degrees of the missing angle.
+	 */
+	public static int missingInteriorAngle(int angle1, int angle2) {
+		return 180 - (angle1 + angle2);
+	}
+	
+	/**
+	 * Finds the missing interior angle of a triangle.
+	 * @param angle1 The degrees of one of the given angles.
+	 * @param angle2 The degrees of the other given angle.
+	 * @return The degrees of the missing angle.
+	 */
+	public static float missingInteriorAngle(float angle1, float angle2) {
+		return 180 - (angle1 + angle2);
+	}
+	
+	/**
+	 * Finds the missing interior angle of a triangle.
+	 * @param angle1 The degrees of one of the given angles.
+	 * @param angle2 The degrees of the other given angle.
+	 * @return The degrees of the missing angle.
+	 */
+	public static double missingInteriorAngle(double angle1, double angle2) {
+		return 180 - (angle1 + angle2);
+	}
+	
+	/**
+	 * Finds the missing interior angle of a triangle.
+	 * @param angle1 The degrees of one of the given angles.
+	 * @param angle2 The degrees of the other given angle.
+	 * @return The degrees of the missing angle.
+	 */
+	public static long missingInteriorAngle(long angle1, long angle2) {
+		return 180 - (angle1 + angle2);
+	}
 }
